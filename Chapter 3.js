52407a80b8f4b64bd51bee52b651f5f2ec610ee5
kimyee <kimyee.16@gmail.com>
2014-10-15 21:55:24 -0400

    Answer to Question Recursion

--- Chapter 3.js
+++ Chapter 3.js
@@ -7,3 +7,18 @@ return y;
 }
 console.log(min(0, 10));
 console.log(min(0, -10));
+
+//Recursion
+function isEven(n){
+if(n==0)
+return true;
+else if (n==1)
+return false;
+else if (n>1)
+return isEven(n-2)
+else
+return isEven(n+2);
+}
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
