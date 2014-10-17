9f53f16a377c87a04fec6cb0b8b61eefad6c0def
kimyee <kimyee.16@gmail.com>
2014-10-17 18:01:28 -0400

    indentation

--- Chapter 2.js
+++ Chapter 2.js
@@ -2,20 +2,19 @@
 var sign = "#";
 var counter = 1;
 while (counter <=7){
-console.log(sign);
-sign = sign + "#";
-counter++;
-
+	console.log(sign);
+	sign = sign + "#";
+	counter++;
 }
 
 // FizzBuzz
 var counter = 1;
 while(counter <=100){
-if(counter %3 == 0 && counter %5 == !0) console.log("Fizz");
-else if(counter %5 == 0 && counter %3 == !0) console.log("Buzz");
-else if (counter %3 == 0 && counter %5 ==0) console.log("FizzBuzz");
-else console.log(counter);
-counter++;
+	if(counter %3 == 0 && counter %5 == !0) console.log("Fizz");
+	else if(counter %5 == 0 && counter %3 == !0) console.log("Buzz");
+	else if (counter %3 == 0 && counter %5 ==0) console.log("FizzBuzz");
+	else console.log(counter);
+	counter++;
 }
 
 //Chess Board
@@ -28,6 +27,6 @@ String.prototype.repeat = function(n) {
     return new Array(1 + (n || 0)).join(this);
 }
 for (var number = 1; number <= size/2; number++){
-console.log(hashSpace.repeat(size));
-console.log(spaceHash.repeat(size));
+	console.log(hashSpace.repeat(size));
+	console.log(spaceHash.repeat(size));
 }
--- Chapter 3.js
+++ Chapter 3.js
@@ -1,24 +1,25 @@
 //Minimum
 function min(x, y){
-if(x < y)
-return x;
-else
-return y;
+	if(x < y)
+		return x;
+	else
+		return y;
 }
 console.log(min(0, 10));
 console.log(min(0, -10));
 
 //Recursion
 function isEven(n){
-if(n==0)
-return true;
-else if (n==1)
-return false;
-else if (n>1)
-return isEven(n-2)
-else
-return isEven(n+2);
+	if(n==0)
+		return true;
+	else if (n==1)
+		return false;
+	else if (n>1)
+		return isEven(n-2)
+	else
+		return isEven(n+2);
 }
 console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
+
