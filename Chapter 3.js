1aff3d2cc683e587b0363897634e03a64215bc88
kimyee <kimyee.16@gmail.com>
2014-10-21 23:05:45 -0400

    Complete Bean counting part 1

--- Chapter 3.js
+++ Chapter 3.js
@@ -23,3 +23,23 @@ console.log(isEven(50));
 console.log(isEven(75));
 console.log(isEven(-1));
 
+//Bean Counting
+function countBs(x){
+	var counter = 0;
+	var x_array = x.split("");
+	function logArrayElements(element, index, array){
+		// console.log(index + ": " + element );
+		if(element == "B"){
+			// console.log("true");
+			counter+=1;
+			// console.log("Number of uppercase Bs: " + counter);
+
+		}else{
+			// console.log("false");
+		}
+	}
+	x_array.forEach(logArrayElements);
+	console.log("Number of uppercase Bs: " + counter);
+}
+
+console.log(countBs("BBC"));
