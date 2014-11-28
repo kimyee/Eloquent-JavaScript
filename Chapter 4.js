f125a98b97c9844509b13233631dbc9d7089d473
kimyee <kimyee.16@gmail.com>
2014-11-28 13:29:29 -0500

    Sum of range - bonus question

--- Chapter 4.js
+++ Chapter 4.js
@@ -2,13 +2,33 @@
 function addition(start, end){
 	var numList = [];
 	var total = 0;
-	for(x = 0; x <= 10; x++){
+	for(var x = start; x <= end; x++){
 		numList.push(x);
 	}
-	for(i = 0; i < numList.length; i++){
+	for(var i = 0; i < numList.length; i++){
 		total += numList[i];
 	}
 	console.log(total);
+	console.log("--------------------------------------");
 }
 
 addition(0, 10)
+
+
+//The sum of a range -- bonus
+function display(start, end, step){
+	var numList = [];
+	var i = 0;
+	var revisedNumList = [];
+	for(var x = start; x <= end; x++){
+		numList.push(x);
+	}
+	for(var i=0; i<10; i=i+step){
+		revisedNumList.push(numList[i]);
+	}
+	console.log(numList);
+	console.log(revisedNumList);
+
+}
+
+display(1, 10, 2)
